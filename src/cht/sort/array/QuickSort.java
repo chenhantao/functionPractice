8933ce6d75c10f52d53fc12b79834df5bd29a547
chenhantao <hantaochen@alog.com>
2019-08-29 17:38:47 +0800

    快排

--- src/cht/sort/array/QuickSort.java
+++ src/cht/sort/array/QuickSort.java
@@ -1,10 +1,48 @@
 package cht.sort.array;
 
 /**
- * TODO
+ * 快速排序
  *
  * @author chenhantao
  * @since 2019/8/28
  */
 public class QuickSort {
+    /**
+     * 快速排序算法
+     *
+     * @param array
+     * @param <E>
+     */
+    public static <E extends Comparable<E>> void quickSort(E[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static <E extends Comparable<E>> void quickSort(E[] array, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+
+    }
+
+    private static <E extends Comparable<E>> int partition(E[] array, int start, int end) {
+        // 取第一个为基准
+        E pivot = array[start];
+        int left = start;
+        int right = end;
+
+        int index = start;
+
+        while (right >= left) {
+            if (array[right].compareTo(pivot) < 0) {
+                array[left] = array[right];
+                index = right;
+                left++;
+                break;
+            }
+            right--;
+        }
+
+        while (right)
+    }
 }
