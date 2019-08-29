8884506678bcb33596c0d2573266dd971fb667e3
chenhantao <hantaochen@alog.com>
2019-08-29 14:41:57 +0800

    插入排序

--- src/cht/sort/SortTest.java
+++ src/cht/sort/SortTest.java
@@ -3,6 +3,7 @@ package cht.sort;
 import java.util.Arrays;
 
 import cht.sort.array.BubbleSort;
+import cht.sort.array.InsertionSort;
 
 /**
  * 随便测试啦
@@ -13,14 +14,17 @@ import cht.sort.array.BubbleSort;
 public class SortTest {
     public static void main(String[] args) {
         int[] a = new int[]{1, 3, 5, 11, 32, 13, 51, 12, 53};
-
         Integer[] b = new Integer[a.length];
         for (int i = 0; i < a.length; i++) {
             b[i] = a[i];
         }
 
-        BubbleSort.cockTailSortUpdate(b);
+        InsertionSort.insertionSort(b);
         System.out.println(Arrays.toString(b));
+
+        String[] test = new String[]{"ad", "asdsa", "scve", "ascfq", "sssss", "apsdnmkl"};
+        BubbleSort.cockTailSortUpdate(test);
+        System.out.println(Arrays.toString(test));
     }
 
 
--- src/cht/sort/array/InsertionSort.java
+++ src/cht/sort/array/InsertionSort.java
@@ -8,6 +8,25 @@ package cht.sort.array;
  */
 public class InsertionSort {
     public static <E extends Comparable<E>> void insertionSort(E[] array) {
+        if (array.length == 0) {
+            System.out.println("数组为空");
+            return;
+        }
 
+        long start = System.currentTimeMillis();
+        // 初始选择第一个元素作为有序区
+        for (int i = 1; i < array.length; i++) {
+            E insertValue = array[i];
+
+            int j;
+            // 取无序区最左边的一个和有序区最右边的做比较
+            for (j = i - 1; j >= 0 && (insertValue.compareTo(array[j]) < 0); j--) {
+                // 因为上面已经暂存了要插入的值，因此将有序区的元素后移不交换
+                array[j + 1] = array[j];
+            }
+            // 不是很好表达，画个图就知道了
+            array[j + 1] = insertValue;
+        }
+        System.out.println("耗时: " + (System.currentTimeMillis() - start) + "ms");
     }
 }
