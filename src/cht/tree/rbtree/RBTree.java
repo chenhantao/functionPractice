c9a9ec5f15d3d2de6752e408070215e840bcad02
chenhantao <hantaochen@alog.com>
2019-09-30 16:44:31 +0800

    改名字

--- src/cht/tree/rbtree/RBTree.java
+++ src/cht/tree/rbtree/RBTree.java
@@ -445,7 +445,7 @@ public class RBTree<T extends Comparable<T>> {
         return node;
     }
 
-    public void proOrder() {
+    public void preOrder() {
         this.preOrder(this.root);
     }
     private void preOrder(RBTNode<T> node) {
@@ -481,7 +481,7 @@ public class RBTree<T extends Comparable<T>> {
 
     public void order() {
         System.out.println("前序遍历: ");
-        this.proOrder();
+        this.preOrder();
         System.out.println();
 
         System.out.println("中序遍历: ");
